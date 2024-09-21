ee67d02d6e1a1261c95ae0f1cd0397a70bb5485d
Sarah Rollins <s.rollins751@gmail.com>
2024-09-21 11:51:09 +0900

    Adjusted asset prefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   assetPrefix: process.env.NODE_ENV === 'production'
-  ? 'https://codesmash-aws-serverless-web-ssr.s3.us-east-2.amazonaws.com'
+  ? 'https://codesmash-ssr-static.s3.us-east-1.amazonaws.com'
   : '',
   images: {
     unoptimized: true,
